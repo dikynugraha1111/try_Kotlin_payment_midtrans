f16d1465516900d02c462314907aaa23606ee9ff
Diky <dikynugraha1111@gmail.com>
2021-05-08 15:47:59 +0700

    Add
    - Merchant Base URL to localhost
    - create transaction request
    - create item details
    - create customer details

--- app/src/main/java/com/example/try_midtrans/MainActivity.kt
+++ app/src/main/java/com/example/try_midtrans/MainActivity.kt
@@ -3,9 +3,13 @@ package com.example.try_midtrans
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.midtrans.sdk.corekit.callback.TransactionFinishedCallback
+import com.midtrans.sdk.corekit.core.TransactionRequest
 import com.midtrans.sdk.corekit.core.themes.CustomColorTheme
+import com.midtrans.sdk.corekit.models.ItemDetails
 import com.midtrans.sdk.corekit.models.snap.TransactionResult
 import com.midtrans.sdk.uikit.SdkUIFlowBuilder
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.ArrayList
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,11 +24,32 @@ class MainActivity : AppCompatActivity() {
                 result ->
                 //logic for transaction result
             })
-            .setMerchantBaseUrl("") //Isi dengan file PHP (fork di git) dan telah di hosting di web. Cantumkan URL disini
+            .setMerchantBaseUrl("http://10.0.2.2/payment_gateway/payment.php") //Isi dengan file PHP (fork di git) dan telah di hosting di web. Cantumkan URL disini
             .enableLog(true)
             .setColorTheme(CustomColorTheme("#FFE51255", "#B61548", "#FFE51255"))
             .setLanguage("id")
             .buildSDK()
+
+        pesan.setOnClickListener {
+            //Declare Variable's
+            val harga = 20000
+            val quantity = jumlah.text.toString()
+            val total = harga.toDouble() * quantity.toDouble()
+            val catatan = catatan.text.toString()
+            //Create Transaction Request
+            val transactionRequest = TransactionRequest("Diky-Store"+System.currentTimeMillis().toShort()+"",total)
+            //Create Item Detail's
+            val item = ItemDetails("NamaItemId",harga.toDouble(),quantity.toInt(),"KOTLIN")
+            val itemDetails = ArrayList<ItemDetails>()
+            itemDetails.add(item)
+            //Create Customer Detail Object
+            uiKitsDetails()
+
+        }
+    }
+
+    fun uiKitsDetails(){
+        
     }
 
 }
